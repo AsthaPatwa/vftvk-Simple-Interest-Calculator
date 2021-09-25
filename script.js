c817b008f3c9d0dad06997f1ef0562ca866484a8
AsthaPatwa <87671059+AsthaPatwa@users.noreply.github.com>
2021-09-25 13:38:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,47 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+body{
+    background-color: black;
+    font-family: Arial;
+    color: white;
 }
-        
+h1{
+    color: gray;
+    font-family: Verdana;
+}
+.maindiv{
+    background-color: white;
+    color: black;
+    width: 500px;
+    padding: 20px;
+    border-radius: 25px;
+    margin-left: auto;
+    margin-right: auto;
+}
+.contents{
+    margin-left: 80px ;
+}
+table td{
+    padding-bottom: 10px;
+}
+#compute{
+    margin-bottom: 10px;
+    color: black;
+}
+#principal{
+    height: 25px;
+    width: 190px;
+}
+
+#footer
+{
+    margin-top: 10px;
+}
+
+.labels
+{
+    width: 120px;
+}
+
+.highlight
+{
+    background-color: yellow;
+}
